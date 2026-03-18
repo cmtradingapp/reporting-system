5d4c02dc9f81679d0842025999ae727584934503
Elise I | CMTrading <elise.i@cmtrading.com>
2026-03-18 15:09:39 +0200

    Remove temp rerun snapshot script

--- rerun_snapshot.py
+++
@@ -1,8 +0,0 @@
-import sys
-sys.path.insert(0, '/app')
-from app.etl.fetch_and_store import run_daily_equity_zeroed_snapshot
-
-dates = ['2026-03-14', '2026-03-15', '2026-03-16', '2026-03-17', '2026-03-18']
-for d in dates:
-    result = run_daily_equity_zeroed_snapshot(snapshot_date=d)
-    print(result)
